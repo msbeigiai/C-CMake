a34000bb435960a0acda311dc94d7cb41fdbd250
msbeigiai <msbeigi83@gmail.com>
2023-12-26 14:59:37 +0200

    Add confurable values

--- HelloWorld/app/main.cc
+++ HelloWorld/app/main.cc
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "my_lib.h"
+#include "config.hpp"
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    std::cout << project_name << "\n";
+    std::cout << project_version << "\n";
+    print_hello_world();
+
+
     return 0;
 }
