4195ebfc68d43d6f8c2224db6e5e309577d200c6
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2023-12-28 18:23:23 +0200

    Add documentation

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['set_0',['set',['../my__lib_2_c_make_lists_8txt.html#aa9a494c664f262ecea24be0c1a0270c4',1,'CMakeLists.txt']]]
+];
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['add_5flibrary_0',['add_library',['../my__lib_2_c_make_lists_8txt.html#abe601df9e07c9fdb74b07d4a38174a2e',1,'CMakeLists.txt']]]
+];
